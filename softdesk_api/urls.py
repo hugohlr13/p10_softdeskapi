01c1fbb75c7838e8344af9f93b1529d55bcf18d6
hugohlr13 <hugo.huetleroy@gmail.com>
2023-06-29 01:12:29 +0200

    Test Users ok

--- softdesk_api/urls.py
+++ softdesk_api/urls.py
@@ -19,6 +19,7 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from users.views import UserViewSet
 
+
 router = DefaultRouter()
 router.register(r"users", UserViewSet)
 
