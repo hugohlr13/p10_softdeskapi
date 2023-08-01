d811460115e1404f392ea7adee0c0fff94e30084
hugohlr13 <hugo.huetleroy@gmail.com>
2023-08-02 00:07:42 +0200

    Add permission contributor

--- contributors/views.py
+++ contributors/views.py
@@ -1,7 +1,17 @@
 from rest_framework import viewsets
+from rest_framework.exceptions import PermissionDenied
 from .models import Contributor
+from api.models import Project
 from .serializers import ContributorSerializer
 
 class ContributorViewSet(viewsets.ModelViewSet):
     queryset = Contributor.objects.all()
     serializer_class = ContributorSerializer
+
+    def perform_create(self, serializer):
+        project = self.request.data.get('project_id')
+        # Assurez-vous que l'utilisateur qui fait la requête est l'auteur du projet
+        if Project.objects.get(id=project).author_user_id == self.request.user:
+            serializer.save()
+        else:
+            raise PermissionDenied("Vous devez être l'auteur du projet pour ajouter des contributeurs.")
