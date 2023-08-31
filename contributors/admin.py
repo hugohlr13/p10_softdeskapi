e4bdca27aeaad2723dc41d7e107cd8c07d8dd29f
hugohlr13 <hugo.huetleroy@gmail.com>
2023-08-31 12:51:17 +0200

    Admin Contributors

--- contributors/admin.py
+++ contributors/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
 from .models import Contributor
+from django.contrib.auth.models import User  # Si c'est le modèle que vous utilisez pour l'authentification
+from api.models import Project  # Import du modèle Project
 
-admin.site.register(Contributor)
+class ContributorAdmin(admin.ModelAdmin):
+    list_display = ('user_id', 'project_id',)  # Assurez-vous que ces noms de champs correspondent à ceux dans votre modèle
+    list_filter = ('project_id',)  # Le filtre par projet
 
+admin.site.register(Contributor, ContributorAdmin)
